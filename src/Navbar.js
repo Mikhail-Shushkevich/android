84cf387b26c17e4c093fa54db5223696dbc1c5b4
Mikhail <1702948@gmail.com>
2024-05-08 23:01:04 +0200

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export const Navbar = props => {
+    return (
+        <View style={SVGAnimatedLengthList.navbar}>
+            <Text style={styles.text}>Todo App</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create ({
+    navbar: {
+
+    },
+    text: {
+
+    }
+})
